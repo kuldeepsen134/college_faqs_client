2ddd99eefbb319dd8db8969382d13adf96eade03
Kuldeep Sen <kuldeepsen134@gmail.com>
2023-11-30 22:40:55 +0530

    Add comapare collage api in compaire component

--- src/Pages/CustomPages.js
+++ src/Pages/CustomPages.js
@@ -1,18 +1,29 @@
+import { useNavigate, } from "react-router-dom";
 import BannersList from "../Components/BannersList";
 import "../index.css";
 import { useEffect } from "react";
 
 const CustomPage = (props) => {
+
   useEffect(() => {
-    window.scrollTo(0,0);
-  },[])
-  
+    window.scrollTo(0, 0);
+  }, [])
+
+
+  let token = localStorage.getItem('token')
+
+  const navigate = useNavigate()
+
+  if (!token) {
+    navigate('/login')
+  }
   return (
     <>
       <section className="hero-banner">
         <div className="container-fluid">
           <div className="row">
             <div className="col-12 p-0">
+
               <img
                 src="./img/college-banner.jpg"
                 alt="Hero banner"
@@ -52,7 +63,7 @@ const CustomPage = (props) => {
               {props.children}
             </div>
             <div className="col-12 col-sm-12 col-md-4">
-              <BannersList custom={true} />
+              {!token ? navigate('/login') : <BannersList custom={true} />}
             </div>
           </div>
         </div>
--- src/Pages/compairCollage.js
+++ src/Pages/compairCollage.js
@@ -3,22 +3,25 @@ import { useNavigate, useParams, } from "react-router-dom";
 import axios, { instance } from "../api/axios";
 import { STATIC_URL } from "../config/config";
 import useAuth from "../hooks/useAuth";
+import { useDispatch } from "react-redux";
+import { headerSearch } from "../redux/commonSlice/commonSlice";
 
 const CompairCollage = () => {
   const { id } = useParams()
 
   const [collageIDs, setCollageIDs] = useState([])
-
   const [getCollage, setCollage] = useState({})
-
   const [show, setShow] = useState(false)
 
-  const { auth } = useAuth();
+  const [query, setQuery] = useState("");
+  const dispatch = useDispatch()
 
   let token = localStorage.getItem('token')
   const navigate = useNavigate()
 
+  const [searchResults, setSearchResults] = useState([]);
 
+  const [selectedId, setSelectedId] = useState(null)
 
   useEffect(() => {
     (async () => {
@@ -33,109 +36,163 @@ const CompairCollage = () => {
   }, [])
 
 
+  useEffect(() => {
+    let timeoutId;
+
+    const fetchData = async () => {
+      try {
+        const response = await dispatch(headerSearch({ q: query }));
+        setSearchResults(response?.payload?.data?.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(fetchData, 500);
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [dispatch, query]);
 
 
+
+
+  const handleResultClick = (id) => {
+    setSelectedId(id);
+  };
 
 
   return (
     <>
-      <section id="latest-updates" className="media-5 compare-design custom-inner-pages position-relative">
+      <section id="latest-updates" className="media-5 compare-design custom-inner-pages">
         <div className="container">
 
-          <div className="row">
+          {!show && <div className="row">
 
-            
-              <div className="card-group">
+            {/* {!show && */}
+            <div className="card-group">
 
-                <div className="card">
-                  {getCollage?.baseCollage?.map((item, i) => {
-                    return (
-                      <div key={i}>
-                        <img src={STATIC_URL + "/images/" + item.college_image} alt="image" />
-                        <div className="card-body">
-                          <h5 style={{ color: '#1048c3' }} className="institute-name px-3">{item?.college_name}</h5>
-                          <p className="location px-3">
-                            {item?.location}</p>
-                          <p className="course-name px-3">{item?.program}</p>
+              <div className="card">
+                {getCollage?.baseCollage?.map((item, i) => {
+                  return (
+                    <div key={i}>
+                      <img src={STATIC_URL + "/images/" + item.college_image} alt="image" />
+                      <div className="card-body">
+                        <h5 style={{ color: '#1048c3' }} className="institute-name px-3">{item?.college_name}</h5>
+                        <p className="location px-3">
+                          {item?.location}</p>
+                        <p className="course-name px-3">{item?.program}</p>
 
-                          {/* <button style={{ color: 'blue', border: 'solid' }}>Modify Selection</button> */}
-                          <hr />
-                          <h6 className="text-center">Institute Information</h6>
-                          <hr />
-                          <div className="px-3">
-                            <p style={{ display: 'flex', justifyContent: 'space-between' }}>Established Year <span>{item?.established_year}</span></p>
-                            <p style={{ display: 'flex', justifyContent: 'space-between' }}>Ranking<span>{item?.ranking}</span> </p>
-                            <p style={{ display: 'flex', justifyContent: 'space-between' }}>Ownership<span>Public/Government, Autonomous</span> </p>
-                          </div>
-                          <hr />
-                          <h6 className="text-center">Course Details</h6>
-                          <hr />
-                          <div style={{ display: 'flex', justifyContent: 'space-between' }} className="px-3">
-                            <p style={{ color: '#000' }}><strong>Total Courses <span> (11)</span></strong></p>
-                          </div>
+                        {/* <button style={{ color: 'blue', border: 'solid' }}>Modify Selection</button> */}
+                        <hr />
+                        <h6 className="text-center">Institute Information</h6>
+                        <hr />
+                        <div className="px-3">
+                          <p style={{ display: 'flex', justifyContent: 'space-between' }}>Established Year <span>{item?.established_year}</span></p>
+                          <p style={{ display: 'flex', justifyContent: 'space-between' }}>Ranking<span>{item?.ranking}</span> </p>
+                          <p style={{ display: 'flex', justifyContent: 'space-between' }}>Ownership<span>Public/Government, Autonomous</span> </p>
+                        </div>
+                        <hr />
+                        <h6 className="text-center">Course Details</h6>
+                        <hr />
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }} className="px-3">
+                          <p style={{ color: '#000' }}><strong>Total Courses <span> (11)</span></strong></p>
+                        </div>
 
-                          <div className="px-3">
-                            <p>B.Tech</p>
-                          </div>
+                        <div className="px-3">
+                          <p>B.Tech</p>
                         </div>
                       </div>
-                    )
-                  })}
-                </div>
-
-
-                {/* Card Number 11111 */}
-
-                {<div className="card">
-                  <div className="add-collage mt-6 text-center">
-                    <div className=" custom-border">
-                      <button type="button"
-                        onClick={() => setShow(!show)}
-                        className="btn add-collage-button mt-4">
-                        <span className="plus"> +</span>
-                        <span>
-                          Add College
-                        </span>
-                      </button>
                     </div>
-                    <span className="another-addition">Or</span>
-                    <button className="btn similar-button" type="button">Add Similar College</button>
+                  )
+                })}
+              </div>
+
+
+              {/* Card Number 11111 */}
+
+              {<div className="card">
+                <div className="add-collage mt-6 text-center">
+                  <div className=" custom-border">
+                    <button type="button"
+                      onClick={() => setShow(!show)}
+                      className="btn add-collage-button mt-4">
+                      <span className="plus"> +</span>
+                      <span>
+                        Add College
+                      </span>
+                    </button>
                   </div>
+                  <span className="another-addition">Or</span>
+                  <button className="btn similar-button" type="button">Add Similar College</button>
                 </div>
-                }
-                {/* Card Number 2222 */}
-                <div className="card">
-                  <div className="add-collage mt-6 text-center">
-                    <div className=" custom-border">
-                      <button type="button" className="btn add-collage-button mt-4">
-                        <span className="plus"> +</span>
-                        <span>
-                          Add College
-                        </span>
-                      </button>
-                    </div>
-                    <span className="another-addition">Or</span>
-                    <button className="btn similar-button" type="button">Add Similar College</button>
+              </div>
+              }
+              {/* Card Number 2222 */}
+              <div className="card">
+                <div className="add-collage mt-6 text-center">
+                  <div className=" custom-border">
+                    <button type="button" className="btn add-collage-button mt-4">
+                      <span className="plus"> +</span>
+                      <span>
+                        Add College
+                      </span>
+                    </button>
+                  </div>
+                  <span className="another-addition">Or</span>
+                  <button className="btn similar-button" type="button">Add Similar College</button>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          }
+
+          {show &&
+            <>
+              <div className="row">
+                <div className="col-4">
+                  <button type="button" onClick={() => setShow(!show)}>
+                    X
+                  </button>
+                  <div>
+                    <input
+                      type="search"
+                      id="search"
+                      placeholder="Search Colleges"
+                      value={query}
+                      onChange={(e) => setQuery(e.target.value)}
+                      style={{
+                        width: "84%",
+                        padding: "10px",
+                        borderRadius: "8px",
+                        backgroundColor: "white",
+                        textAlign: "center "
+                      }}
+                    />
+                  </div>
+
+                  <div>
+                    {searchResults?.map((result, index) => (
+                      <div key={index}>
+                        <p onClick={() => handleResultClick(result.id)}>{result.name}</p>
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="col-8">
+                    <p>Selected ID: {selectedId}</p>
                   </div>
                 </div>
               </div>
-          
-
-          </div>
-
-          {show &&
-           <div className="overlap-card">
-            <button type="button" onClick={() => setShow(!show)}>
-              X
-            </button>
-
-            <input type="search"
-              placeholder="Collage name here"
-            >
-
-            </input>
-          </div>
-
+            </>
           }
 
         </div>
